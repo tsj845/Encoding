25214047947e35e899cd78cac2705d1b0b2796de
Tristan <tschartman41@gmail.com>
2021-12-21 14:38:17 -0500

    made the autogenerated trees better

--- main.py
+++ main.py
@@ -1,25 +1,87 @@
+from numpy import floor
+
 class BitTree ():
     """
     tree that maps bits to characters
     """
     def __init__ (self, data):
+        # use this to configure how final swaps are done
+        self._swap_policy = 1
         self.tree = data
         if (type(data) == str):
+            print("BOOMBAH")
             self.tree = self._construct(data)
+            self._swapbottom(data)
+            print("DBP")
+            print(self.tree)
+    def _fit (self, tree, char):
+        for i in range(len(tree)):
+            if (type(tree[i]) != str):
+                found, ret = self._fit(tree[i], char)
+                if (found):
+                    return True, str(i)+ret
+            else:
+                if (tree[i] == char):
+                    return True, str(i)
+        return False, ""
+    def find (self, char):
+        return self._fit(self.tree, char)[1]
+    def _swap (self, l, i1=0, i2=1):
+        l = l.copy()
+        h = l[i1]
+        l[i1] = l[i2]
+        l[i2] = h
+        return l
+    def _getdepth (self, lst, depth=0):
+        depths = []
+        for i in range(len(lst)):
+            if (type(lst[i]) != str):
+                depths.append(self._getdepth(lst[i], depth+1))
+        return max(depths) if len(depths) > 0 else depth
+    def _swaplayers (self, lst, pi=0):
+        for i in range(len(lst)):
+            if (type(lst[i]) != str and self._getdepth(lst[i]) > 2 and pi % 2 == 0):
+                lst[i] = self._swaplayers(lst[i], i)
+        return self._swap(lst)
+    def _swapbottom (self, org):
+        swapped = ""
+        for i in range(0, len(org), 2):
+            if (org[i] in swapped):
+                continue
+            ni = (i + 1) % len(org)
+            if (self._swap_policy == 0):
+                ni = (i + ord(org[i])) % len(org)
+            elif (self._swap_policy == 1):
+                ni = (i + len(org) // 2) % len(org)
+            elif (self._swap_policy == 2):
+                ni = (i + round(floor(len(org)/10))-5) % len(org)
+            swapped += org[i]
+            swapped += org[ni]
+            print(org[i], org[ni], "SWAP")
+            p1 = self.find(org[i])
+            p2 = self.find(org[ni])
+            h = org[ni]
+            self[p2] = org[i]
+            self[p1] = h
     def _construct (self, data):
         f = list(data)
+        # print(f)
         while len(f) > 2:
             b = []
-            for i in range(len(f)):
-                if (i % 2 == 0):
-                    continue
-                b.append([f[i-1],f[i]])
-            if (f[-1] not in b[-1]):
-                b.append(f[-1])
+            for i in range(2, len(f), 3):
+                b.append([f[i-2], [f[i-1], f[i]]])
+            if (len(f) % 3 > 0):
+                b.append([*f[len(f)-(len(f)%3):]] if len(f)%3 > 1 else f[-1])
             f = b
+        while len(f) == 1:
+            f = f[0]
+        f = self._swaplayers(f)
+        print(f)
         return f
     def __getitem__ (self, path):
         s = self.tree
+        if (type(path) == int):
+            return s[path]
         for c in path:
             s = s[int(c)]
         return s
@@ -30,6 +92,8 @@ class BitTree ():
                 break
             s = s[int(c)]
         s[int(path[-1])] = value
+    def __repr__ (self):
+        return self.tree.__repr__()
 
 class Encoder ():
     """
@@ -37,27 +101,25 @@ class Encoder ():
     """
     def __init__ (self):
         self.protocols = {
-            "bintree" : {
-                "id" : 0xadfc,
+            0xadfc : {
                 "versions" : [0x1498],
                 "data" : {
-                    0x1498 : "abcdefgh"
+                    0x1498 : {"tree":BitTree("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.!? ")}
                 }
             },
-            "hextable" : {
-                "id" : 0xa7e4,
+            0xa7e4 : {
                 "versions" : [0x2048],
                 "data" : {
-                    0x2048 : " abcdefghijklmnopqrstuvwxyz"
+                    0x2048 : {"table":" abcdefghijklmnopqrstuvwxyz0123456789,.!?ABCDEFGHIJKLMNOPQRSTUVWXYZ", "width":7}
                 }
             }
         }
+        self.namemap = {"bittree":0xadfc, "hextables":0xa7e4}
     def _compress (self, data):
         trailing = data[len(data)-(len(data)%4):]
         data = data[:len(data)-(len(data)%4)]
         f = ""
         for i in range(0, len(data), 4):
-            print(data[i:i+4])
             f += hex(int(data[i:i+4], base=2))[2:]
         f += ";" + trailing
         return f
@@ -85,33 +147,61 @@ class Encoder ():
         data = b[40:-tbits]
         return pid, vid, data
     def _encode_bt (self, pid, vid, edata, data):
+        tree = edata["tree"]
         encoded = ""
+        for c in data:
+            p = tree.find(c)
+            print(p, c)
+            encoded += p
         header = self._generate_header(pid, vid, encoded)
-    def _decode_bt (self, pid, vid, edata, data):
-        pass
+        return header
+    def _decode_bt (self, edata, data):
+        tree = edata["tree"]
+        f = ""
+        work = tree
+        for c in data:
+            work = work[int(c)]
+            if (type(work) == str):
+                f += work
+                work = tree
+        return f
     def _encode_xt (self, pid, vid, edata, data):
+        table = edata["table"]
+        width = edata["width"]
         encoded = ""
+        for c in data:
+            encoded += bin(table.index(c))[2:].rjust(width, "0")
         header = self._generate_header(pid, vid, encoded)
-    def _decode_xt (self, pid, vid, edata, data):
-        pass
+        return header
+    def _decode_xt (self, edata, data):
+        table = edata["table"]
+        width = edata["width"]
+        f = ""
+        for i in range(0, len(data), width):
+            f += table[int(data[i:i+width], base=2)]
+        return f
     def encode (self, protocol, version, data):
-        pid = self.protocols[protocol]["id"]
-        vid = self.protocols[protocol]["versions"][version]
-        edata = self.protocols[protocol]["data"][vid]
+        pid = self.namemap[protocol] if type(protocol) == str else protocol
+        vid = self.protocols[pid]["versions"][version]
+        edata = self.protocols[pid]["data"][vid]
+        res = "NULL"
         if (pid == 0xadfc):
-            self._encode_bt(pid, vid, edata, data)
+            res = self._encode_bt(pid, vid, edata, data)
         elif (pid == 0xa7e4):
-            self._encode_xt(pid, vid, edata, data)
-    def decode (self, protocol, version, data):
-        pid = self.protocols[protocol]["id"]
-        vid = self.protocols[protocol]["versions"][version]
-        edata = self.protocols[protocol]["data"][vid]
+            res = self._encode_xt(pid, vid, edata, data)
+        return res
+    def decode (self, data):
+        pid, vid, data = self._unpack_data(data)
+        edata = self.protocols[pid]["data"][vid]
+        res = "NULL"
         if (pid == 0xadfc):
-            self._decode_bt(pid, vid, edata, data)
+            res = self._decode_bt(edata, data)
         elif (pid == 0xa7e4):
-            self._decode_xt(pid, vid, edata, data)
+            res = self._decode_xt(edata, data)
+        return res
 
 e = Encoder()
 comp = e._compress("00111010")
 print(comp, e._expand(comp))
-x = e._generate_header(0xa7e4, 0x2048, "101010010101")
+x = e.protocols[0xadfc]["data"][0x1498]["tree"]
+# x = e._generate_header(0xa7e4, 0x2048, "101010010101")
